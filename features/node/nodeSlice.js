3c71ff327703902522a25ab7fce688dd070267ad
Casahel <68067947+Casahel@users.noreply.github.com>
2022-08-26 08:47:45 +0200

    Create nodeSlice.js

---
+++ features/node/nodeSlice.js
@@ -0,0 +1,19 @@
+const initialState = {
+    "name": null
+}
+
+export default function nodeReducer ( state = initialState, action ) {
+    switch ( action.type )
+    {
+        case 'LOAD_NODE_TO_EDITOR':
+            return action.payload;
+
+       /* case 'NODE/UPDATE_NODE':
+            return (
+                state, action.payload
+            )*/
+        
+        default:
+            return state;
+    }
+}
