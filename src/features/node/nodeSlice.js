163a60f5e536f4394658e53ea63c90f21cb54a61
Casahel <68067947+Casahel@users.noreply.github.com>
2022-09-02 21:30:25 +0200

    Add files via upload

--- src/features/node/nodeSlice.js
+++ src/features/node/nodeSlice.js
@@ -5,13 +5,8 @@ const initialState = {
 export default function nodeReducer ( state = initialState, action ) {
     switch ( action.type )
     {
-        case 'LOAD_NODE_TO_EDITOR':
+        case 'NODE/LOAD_TO_EDITOR':
             return action.payload;
-
-        /* case 'NODE/UPDATE_NODE':
-             return (
-                 state, action.payload
-             )*/
 
         default:
             return state;
